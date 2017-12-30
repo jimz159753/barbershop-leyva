b1dec0dcbea6e7820a6bc7845e2f8c3d38d123ff
jimz <luisjc140992@gmail.com>
2017-12-30 02:18:36 -0600

    adding new sections and images

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="header flex shadow-md w-full">
+    <div class="header shadow-md w-full">
       <button type="button" class="pin-l absolute bg-grey-darker hover:bg-orange-ligth text-white font-semibold hover:text-white py-2 px-4">
         |||
       </button>
@@ -12,7 +12,7 @@
         <div class="info flex justify-center">
 
 
-          <div class="max-w-md w-full lg:flex h-64">
+          <div class="max-w-md w-full lg:flex h-64 shadow-md">
             <div class="h-full lg:w-1/4 flex-none bg-cover text-center overflow-hidden" style="background-image: url('https://i.pinimg.com/736x/16/4f/a9/164fa9b67254a1cdd0dd041f10848f13--vintage-barber-barbers.jpg')">
             </div>
             <div class="h-full lg:w-1/4 flex-none bg-cover text-center overflow-hidden mx-3" style="background-image: url('https://i.pinimg.com/736x/16/4f/a9/164fa9b67254a1cdd0dd041f10848f13--vintage-barber-barbers.jpg')">
@@ -27,15 +27,23 @@
 
         </div>
       </div>
-      <div class="testimonials bg-white text-center">
-        <h1>TESTIMONIALS</h1>
+      <div class="testimonials flex justify-center w-full">
+        <h1 class="text-white pt-8">TESTIMONIALS</h1>
       </div>
-      <div class="our-work bg-grey-darker">
-        <h1>OUR-WORK</h1>
+      <div class="our-work bg-white flex justify-center w-full">
+        <h1 class="text-black pt-8">OUR-WORK</h1>
+      </div>
+      <div class="products bg-black flex justify-center w-full">
+        <h1 class="text-white pt-8">PRODUCTS</h1>
       </div>
     </div>
-    <div class="footer bg-grey-dark">
-      <h1>FOOTER</h1>
+    <div class="footer bg-white flex flex-wrap w-full">
+      <div class="maps w-1/2 mr-auto border-r-2 border-black">
+        <h1>MAP</h1>
+      </div>
+      <div class="form w-1/2 ml-auto border-l-2 border-black">
+        <h1>FORM</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -55,8 +63,10 @@ export default {
 <style scoped>
 /*HEADER*/
   .header{
-    background: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url('http://www.petesbarbershop.net/images/petesshopgold-3.jpg') no-repeat !important;
+    background: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url('http://www.petesbarbershop.net/images/petesshopgold-3.jpg') no-repeat center center fixed !important;
+    background-size: cover;
     height: 600px;
+    margin-bottom: 10px;
   }
 
   .left-btn{
@@ -71,28 +81,31 @@ export default {
   }
 
   .body .about{
-    height: 500px;
+    height: 600px;
   }
 
   .body .info{
-    padding-top: 140px;
+    padding-top: 150px;
 
   }
 
   .body .testimonials{
-    width: 1400px;
-    height: 300px;
+    background: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url('http://lamodastyle.com/wp-content/uploads/2014/09/Straight-Razor-Shave-Barber-Toledo.jpg') no-repeat center center fixed !important;
+    background-size: cover;
+    height: 450px;
   }
 
   .body .our-work{
-    width: 1400px;
-    height: 300px;
+    height: 450px;
+  }
+
+  .body .products{
+    height: 450px;
   }
 /*BODY*/
 /*FOOTER*/
   .footer{
-    width: 1400px;
-    height: 100px;
+    height: 350px;
   }
 /*FOOTER*/
 </style>
